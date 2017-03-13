c94bccdb4f6e043d91641b73223c166b9f8d727e
ElmedinaSaldic <elmedinasa@edu.aau.at>
2017-03-13 18:01:37 +0100

    Grobe Implementierungen. Taschenrechner sollte jetzt funktionieren, da die onClick methode implementiert wurde (entsprechende ID´s wurden "geholt"...)

--- app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
@@ -21,4 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
        // System.out.print("Hello World");
     }
+
+    public void MyDividieren (View v){ //OnClick(=MyDividieren) zeigen was passiert wernn man auf den Button klickt
+
+        EditText Divident = (EditText)findViewById(R.id.Divident); //die richtige ID holen (von activity_main
+        EditText Divisor = (EditText)findViewById(R.id.Divisor);  //also Objekt wird angesprochen
+        EditText Ergebnis = (EditText)findViewById(R.id.Ergebnis);
+
+        Double zahl1 = Double.parseDouble(Divident.getText().toString()); //get Text und in Double umwandeln
+        Double zahl2 = Double.parseDouble(Divisor.getText().toString());
+
+        Ergebnis.setText(String.valueOf(zahl1/zahl2)); //Die eigentliche Funktion vom Taschenrechner
+    }
 }
