09b4863e5e38f46ebb0f741f04616c585f80e67f
ElmedinaSaldic <elmedinasa@edu.aau.at>
2017-03-13 11:42:24 +0100

    Layout wurde erstmals gewählt (Design)

--- app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.elmedinasaldic.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("asdfgh");
+        System.out.print("Hello World");
     }
 }
