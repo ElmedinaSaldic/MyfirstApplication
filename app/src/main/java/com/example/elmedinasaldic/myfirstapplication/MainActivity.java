78ccdcb38746712566a726ec8ff7184aed71f25d
ElmedinaSaldic <elmedinasa@edu.aau.at>
2017-03-16 16:11:12 +0100

    new TestBranch

--- app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/elmedinasaldic/myfirstapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +23,31 @@ public class MainActivity extends AppCompatActivity {
        // System.out.print("Hello World");
     }
 
-    public void MyDividieren (View v){ //OnClick(=MyDividieren) zeigen was passiert wernn man auf den Button klickt
+    public void MyDividieren (View v) { //OnClick(=MyDividieren) zeigen was passiert wernn man auf den Button klickt
 
-        EditText DividentZahl = (EditText)findViewById(R.id.DividentZahl); //die richtige ID holen (von activity_main
-        EditText DivisorZahl = (EditText)findViewById(R.id.DivisorZahl);  //also Objekt wird angesprochen
-        EditText Ergebnis = (EditText)findViewById(R.id.Ergebnis);
+        EditText DividentZahl = (EditText) findViewById(R.id.DividentZahl); //die richtige ID holen (von activity_main
+        EditText DivisorZahl = (EditText) findViewById(R.id.DivisorZahl);  //also Objekt wird angesprochen
+        EditText Ergebnis = (EditText) findViewById(R.id.Ergebnis);
 
 
         Double zahl1 = Double.parseDouble(DividentZahl.getText().toString()); //get Text und in Double umwandeln
         Double zahl2 = Double.parseDouble(DivisorZahl.getText().toString());
 
-        Ergebnis.setText(String.valueOf(zahl1/zahl2)); //Die eigentliche Funktion vom Taschenrechner
+        Ergebnis.setText(String.valueOf(zahl1 / zahl2)); //Die eigentliche Funktion vom Taschenrechner
+
+        /*try {
+            Ergebnis = Convert.ToDouble(Console.ReadLine());
+        } catch (Exception e) {
+            Console.WriteLine("Zahl eingeben");
+
+        } */
     }
+
+
+
+    public void MyReset (View view){
+        Button Reset = (Button) view;
+    }
+
+    //test Branch
 }
